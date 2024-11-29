bfd60c4a890fc32a48ee51f68da1f22cb319804f
mfukurow <mfukurow@gmail.com>
2024-11-29 12:46:33 +0900

    create __main__.py

---
+++ labchart_econ/__main__.py
@@ -0,0 +1,14 @@
+"""
+labchart_econ toolbox
+
+efish lab
+Hokkaido University
+Author: Matasaburo Fukutomi
+Email: mfukurow@gmail.com
+Version: 0.1.0
+"""
+
+import labchart_econ as le
+
+if __name__ == "__main__":
+    _, _, _ = le.sync_labchart_econ(save=True)
